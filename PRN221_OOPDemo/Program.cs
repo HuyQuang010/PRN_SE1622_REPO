96b83ddd84b34ae8ee9940f1e5c51721f74a1c60
Huy <Huy@LAPTOP-FV65FTT6>
2022-05-19 15:01:20 +0700

    Add project files.

---
+++ PRN221_OOPDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRN221_OOPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
